78b0f422333fb7afac9a0134181f9d8e5b25be4f
sindhuja kuppusami <sindhu.neyveli@gmail.com>
2021-02-28 08:18:25 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ChatComponent } from './chat/chat.component';
+import { TrainBotComponent } from './train-bot/train-bot.component';
+import { HomeComponent } from './home/home.component';
+import { AgGridComponent } from './common/ag-grid/ag-grid.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ChatComponent,
+    TrainBotComponent,
+    AgGridComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgGridModule.withComponents([]),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'chat', component: ChatComponent },
+      { path: 'train-bot', component: TrainBotComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
